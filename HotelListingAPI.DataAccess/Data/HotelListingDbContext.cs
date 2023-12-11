d887f8aa4f3d9b0c233de47edc3e70a02d47095f
dan_tonye <danthurnye@gmail.com>
2023-12-11 15:09:08 -0500

    Added models and database

---
+++ HotelListingAPI.DataAccess/Data/HotelListingDbContext.cs
@@ -0,0 +1,24 @@
+﻿using HotelListingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListingAPI.DataAccess.Data
+{
+	public class HotelListingDbContext : DbContext
+	{
+		public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+		}
+
+		/// <summary>
+		/// Hotels db set
+		/// </summary>
+		public DbSet<Hotel> Hotels { get; set; }
+
+		/// <summary>
+		/// Countries DB set
+		/// </summary>
+		public DbSet<Country> Countries { get; set; }
+	}
+}
+
--- HotelListingAPI/Program.cs
+++ HotelListingAPI/Program.cs
@@ -1,8 +1,16 @@
 ﻿using Serilog;
 
+using Microsoft.EntityFrameworkCore;
+using HotelListingAPI.DataAccess.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//connection string to Db
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<HotelListingDbContext>(options => options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
